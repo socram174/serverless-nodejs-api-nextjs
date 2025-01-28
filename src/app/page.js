48a9b162e2e1a56e7f08016043b25dc26af77896
socram174 <socram174@hotmail.com>
2025-01-28 00:25:30 -0300

    test

--- src/app/page.js
+++ src/app/page.js
@@ -29,7 +29,7 @@ export default function Home() {
   }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello</h1>
+      <h1>Hello v2</h1>
       <button onClick={handleCreate} className="bg-green-400">Press Me</button>
       {data && JSON.stringify(data)}
     </div>
